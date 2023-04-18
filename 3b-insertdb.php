c664f983e3859e30b14a56de40d370fee21d0048
Carissatasa <carissatasa12@gmail.com>
2023-04-18 11:10:23 +0700

    tugas7

---
+++ 3b-insertdb.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// check connection
+if (!$conn) {
+	die("Connection failed: ".mysqli_connect_error());
+}
+$sql = "INSERT INTO liga (kode, negara, champion)
+VALUES ('Jer', 'Jerman', '4'), ('Spa', 'Spanyol', '3'), ('Eng', 'English', '3')";
+
+if (mysqli_query($conn, $sql)) {
+	echo "Insert into DB success";
+} else {
+	echo "Error inserting into DB: ".mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
